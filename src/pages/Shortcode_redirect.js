b0c8613826df75b3ada1e462d9545f13d6d22d4f
skiislam <sisla016@ucr.edu>
2024-10-29 17:26:53 -0700

    Fixed issue where shortcode kept getting inserted for the same website after each refresh. New issue, double entry for the same website with different shortcodes.

--- src/pages/Shortcode_redirect.js
+++ src/pages/Shortcode_redirect.js
@@ -9,31 +9,40 @@ import { useEffect, useState } from "react";
 export default function Shortcode_redirect(){
     const location = useLocation();
     const userWebsite = location.state
-    const [shortCodeExists, setShortCodeExists] = useState(false);
+    // const [shortCodeExists, setShortCodeExists] = useState(false);
+    const [shortCode, setShortCode] = useState(localStorage.getItem('shortCode') || null);
     const navigate = useNavigate();
-    const newShortCode = nanoid(10);
     useEffect(() => {
+       if (!shortCode) 
+        {
+        const newShortCode = nanoid(10);
         const shortCodeRef = ref(db,'web_id/{newShortCode}');
-        onValue(shortCodeRef, (snapshot) => {
-            if(snapshot.exists()) {
-                setShortCodeExists(true);
-            }
-            else {
-                setShortCodeExists(false);
+        
+        get(shortCodeRef).then((snapshot) => {
+            if(!snapshot.exists()) {
                 setDatabase(newShortCode,userWebsite);
+                setShortCode(newShortCode)
+                localStorage.setItem('shortCode', newShortCode)
             }
+        
         });
+    }
     },[]);
  
-
+const handleGohome = () => {
+    localStorage.removeItem('shortCode');
+    setShortCode(null);
+    navigate('/');
+}
     return (
        <div> 
         <h1>
             SHORT CODE REDIRECT
             {userWebsite}
         </h1>
+        <p>Your Shortened link is: www.testshort.com/{shortCode}</p>
         <p> Ready to go back to home: </p>
-        <button onClick={() => {navigate ('/')}}>Submit</button>
+        <button onClick={handleGohome}>Home</button>
 
         </div> 
     );
